160b89940a1741c5c4b9e81cce27811582f150cd
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-12-02 20:20:52 -0300

    Added user repository and general improvements of interfaces signatures

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -21,8 +21,8 @@ public class AuthenticationController : ControllerBase
         var authResult = _authenticationService.Register(
             request.FirstName, request.LastName, request.Email, request.Password);
 
-        var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName,
-            authResult.Email, authResult.Token);
+        var response = new AuthenticationResponse(authResult.User.Id, authResult.User.FirstName,
+            authResult.User.LastName, authResult.User.Email, authResult.Token);
         return Ok(response);
     }
 
@@ -31,8 +31,8 @@ public class AuthenticationController : ControllerBase
     {
         var authResult = _authenticationService.Login(request.Email, request.Password);
 
-        var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName,
-            authResult.Email, authResult.Token);
+        var response = new AuthenticationResponse(authResult.User.Id, authResult.User.FirstName,
+            authResult.User.LastName, authResult.User.Email, authResult.Token);
         return Ok(response);
     }
 }

0405501d12dbceec6a5d5835cfb1d216522079f0
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-12-03 15:28:49 -0300

    Refactor folder structure inside src folder

--- src/BuberDinner.Api/Controllers/ErrorsController.cs
+++ src/BuberDinner.Api/Controllers/ErrorsController.cs
@@ -10,6 +10,6 @@ public class ErrorsController : ControllerBase
     public IActionResult Error()
     {
         var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-        return Problem(title: exception.Message, statusCode: 400);
+        return Problem(title: exception?.Message, statusCode: 400);
     }
 }
